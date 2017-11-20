5ff3a2b50351a9352160a47cf775ae53430192a8
Caity-G-OConnor <caity.g.oconnor@gmail.com>
2017-11-19 17:46:28 -0700

    Change styling of paragraphs

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var paragraphs = $('p');
+    paragraphs.css({
+        'color': 'MidnightBlue',
+        'backgroundColor': 'LightCyan',
+        'fontFamily': 'Georgia',
+        'width': '90%',
+        'margin': 'auto',
+        'padding': '1%'
+    });
+});
